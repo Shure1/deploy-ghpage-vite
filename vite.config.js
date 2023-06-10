a260723a1b7ab2aad66d10057163064c5e7e1e9a
Shure1 <rodriguezelias485@gmail.com>
2023-06-10 17:14:14 -0400

    base updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://Shure1.github.io/deploy-ghpage-vite/',
 })
